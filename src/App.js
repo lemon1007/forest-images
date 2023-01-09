bcb24134ee541d1a14e3369e8787d468d09147fb
青砚 <lemon19961007@sina.com>
2023-01-09 10:27:05 +0800

    Home,History，About添加lazy加载

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
+import React, {lazy, Suspense} from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import History from './pages/History';
-import About from './pages/About';
+import Loading from './components/Loading';
+
+const Home = lazy(() => import('./pages/Home'));
+const History = lazy(() => import('./pages/History'));
+const About = lazy(() => import('./pages/About'));
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <Routes>
-        <Route path="/" exact element={<Home/>}/>
-        <Route path="/history" element={<History/>}/>
-        <Route path="/about" element={<About/>}/>
-      </Routes>
+      <Suspense fallback={<Loading/>}>
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/history" element={<History/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </Suspense>
       <Footer/>
     </div>
   );
