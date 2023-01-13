07a8d056fa3e32bb87a41e7c709724b7737adc21
青砚 <lemon19961007@sina.com>
2023-01-13 17:30:34 +0800

    init

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -2,13 +2,29 @@ import React from 'react';
 import {observer} from 'mobx-react';
 import {useStores} from '../stores';
 import InfiniteScroll from 'react-infinite-scroller';
+import {List} from 'antd';
 
 const Component = observer(() => {
 
   const {HistoryStore} = useStores();
 
+  const options = {
+
+  }
+
   return (
-    <div>HistoryList</div>
+    <div>
+      <InfiniteScroll>
+        <List dataSource={HistoryStore.list}
+              renderItem={
+                item=>{
+                  <List.item key={item.id}></List.item>
+                }
+              }
+        >
+        </List>
+      </InfiniteScroll>
+    </div>
   );
 });
 
